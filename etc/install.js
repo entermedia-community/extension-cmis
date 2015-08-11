8fcbd704d291d6e71505705624b78d6ddcded1df
Ian Miller <ian@ijsolutions.ca>
2015-08-11 11:04:21 -0400

    Newest code

--- etc/install.js
+++ etc/install.js
@@ -4,7 +4,7 @@ importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
 
-var war = "http://dev.entermediasoftware.com/jenkins/job/app-sundial/lastSuccessfulBuild/artifact/deploy/ROOT.war";
+var war = "http://dev.entermediasoftware.com/jenkins/job/extension-cmis/lastSuccessfulBuild/artifact/deploy/ROOT.war";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
 var web = root + "/WEB-INF";
@@ -20,18 +20,31 @@ unziper.unzip(  tmp + "/ROOT.war",  tmp );
 
 log.add("3. REPLACE LIBS");
 var files = new FileUtils();
-files.deleteMatch( web + "/lib/openedit-intranet*.jar");
-files.deleteMatch( web + "/lib/openedit-events*.jar");
-files.deleteMatch( web + "/lib/ical*.jar");
+files.deleteMatch( web + "/lib/extension-cmis*.jar");
 
-files.copyFileByMatch( tmp + "/WEB-INF/lib/openedit-events*.jar", web + "/lib/");
-files.copyFileByMatch( tmp + "/WEB-INF/lib/ical*.jar", web + "/lib/");
+filles.deleteMatch("activation*.jar");
+filles.deleteMatch("antlr*.jar");
+filles.deleteMatch("antlr*.jar");
+filles.deleteMatch("chemistry*.jar");
+filles.deleteMatch("commons-lang*.jar");
+filles.deleteMatch("jaxb-api*.jar");
+filles.deleteMatch("jaxb-impl*.jar");
+filles.deleteMatch("jaxws-api*.jar");
+filles.deleteMatch("jaxws-rt*.jar");
+filles.deleteMatch("mimepull*.jar");
+filles.deleteMatch("resolver*.jar");
+filles.deleteMatch("saaj-api*.jar");
+filles.deleteMatch("saaj-impl*.jar");
+filles.deleteMatch("slf4j-api*.jar");
+filles.deleteMatch("slf4j-log4j12*.jar");
+filles.deleteMatch("stax-ex*.jar");
+filles.deleteMatch("stax2-api*.jar");
+filles.deleteMatch("streambuffer*.jar");
+filles.deleteMatch("stringtemplate*.jar");
+filles.deleteMatch("woodstox-core-asl*.jar");
 
-log.add("4. UPGRADE BASE DIR");
-files.deleteAll( root + "/WEB-INF/base/events");
-files.copyFiles( tmp + "/WEB-INF/base/events", root + "/WEB-INF/base/events");
 
-log.add("5. CLEAN UP");
-files.deleteAll(tmp);
 
-log.add("6. EVENTS INSTALL COMPLETED");
+
+files.copyFileByMatch( tmp + "/WEB-INF/lib/*.jar", web + "/lib/");
+
